e15f93ed75222879f163721513e885f9f5ea1e91
jitendra-kushavah <jitendra.kushavah@candelatech.com>
2025-11-20 12:03:49 +0000

    Modified allure response attachment
    Signed-off-by: jitendra-kushavah <jitendra.kushavah@candelatech.com>

--- lf_libs/lf_tests.py
+++ lf_libs/lf_tests.py
@@ -4310,7 +4310,8 @@ class lf_tests(lf_libs):
 
                 del_resp = get_target_object.controller_library_object.delete_device_reboot_logs(device_name,
                                                                                               query=delete_query)
-                allure.attach(body=json.dumps(del_resp.json(), indent=4), name="delete_logs_from_gw\n",
+                allure.attach(body=json.dumps(del_resp.json()) if del_resp.text.strip() else del_resp.text,
+                              name="delete_logs_from_gw\n",
                               attachment_type=allure.attachment_type.JSON)
                 if del_resp.status_code == 200:
                     logging.info("Crash logs deleted successfully from GW")
@@ -4330,7 +4331,8 @@ class lf_tests(lf_libs):
                         time.sleep(3)
                         del_resp = get_target_object.controller_library_object.delete_device_reboot_logs(device_name,
                                                                                                          query=delete_query)
-                        allure.attach(body=json.dumps(del_resp.json(), indent=4), name="delete_logs_from_gw\n",
+                        allure.attach(body=json.dumps(del_resp.json()) if del_resp.text.strip() else del_resp.text,
+                                      name="delete_logs_from_gw\n",
                                       attachment_type=allure.attachment_type.JSON)
                         if del_resp.status_code == 200:
                             logging.info("Crash logs deleted successfully from GW")
